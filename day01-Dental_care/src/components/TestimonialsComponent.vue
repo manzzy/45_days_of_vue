ac1230d715ecd0c3a589ea7b023392762b7f60b8
mikegirma82@gmail.com <mikegirma82@gmail.com>
2025-12-07 11:31:41 +0300

    refactor: Testimonail card refactor to use array of testimonies

--- day01-Dental_care/src/components/TestimonialsComponent.vue
+++ day01-Dental_care/src/components/TestimonialsComponent.vue
@@ -7,64 +7,64 @@
             </div>
 
             <ul class="testimonial-list">
-                <li class="testimonial-card">
+                <li class="testimonial-card" v-for="testimonial in testimonials" :key="testimonial.author.name">
                     <p class="testimonial-text">
-                        "The team at DentalCare is amazing! They made me feel so comfortable
-                        during my root canal treatment. Highly recommend!"
+                        "{{ testimonial.text }}"
                     </p>
                     <div class="testimonial-author">
-                        <img src="https://randomuser.me/api/portraits/women/8.jpg" alt="User Photo" />
+                        <img :src="testimonial.author.img_link" :alt="testimonial.author.img_alt" />
                         <div class="author-info">
-                            <span class="author-name">Sarah M.</span>
-                            <span class="author-role">Patient</span>
+                            <span class="author-name">{{ testimonial.author.name }}</span>
+                            <span class="author-role">{{ testimonial.author.role }}</span>
                         </div>
                     </div>
                 </li>
 
-                <li class="testimonial-card">
-                    <p class="testimonial-text">
-                        "I had a fantastic experience at DentalCare. The staff were friendly and professional, the procedure was quick and painless, and my smile has never looked better. Highly recommend!"
-                    </p>
-                    <div class="testimonial-author">
-                        <img src="https://randomuser.me/api/portraits/men/78.jpg" alt="User Photo" />
-                        <div class="author-info">
-                            <span class="author-name">John D.</span>
-                            <span class="author-role">Patient</span>
-                        </div>
-                    </div>
-                </li>
-
-                <li class="testimonial-card">
-                    <p class="testimonial-text">
-                        "As a mom, I felt completely at ease bringing my child to DentalCare. The staff were warm and patient, explained every step, and made the visit stress-free. My child left smiling — we couldn't be happier!"
-                    </p>
-                    <div class="testimonial-author">
-                        <img src="https://randomuser.me/api/portraits/women/68.jpg" alt="User Photo" />
-                        <div class="author-info">
-                            <span class="author-name">Fatima M.</span>
-                            <span class="author-role">Mother</span>
-                        </div>
-                    </div>
-                </li>
-
-                <li class="testimonial-card">
-                    <p class="testimonial-text">
-                        "At 52, I was nervous about visiting the dentist again, but the team treated me like family. They were patient, gentle, and explained everything. I left with a warm smile and a lighter heart — I couldn't be happier!"
-                    </p>
-                    <div class="testimonial-author">
-                        <img src="https://randomuser.me/api/portraits/men/22.jpg" alt="User Photo" />
-                        <div class="author-info">
-                            <span class="author-name">Alex W.</span>
-                            <span class="author-role">Patient</span>
-                        </div>
-                    </div>
-                </li>
             </ul>
         </div>
     </section>
 </template>
 
-<script setup>  </script>
+<script setup> 
+    const testimonials = [
+        {
+            text: "The team at DentalCare is amazing! They made me feel so comfortable during my root canal treatment. Highly recommend!",
+            author: {
+                name: "Sarah M.",
+                role: "Patient",
+                img_link: "https://randomuser.me/api/portraits/women/8.jpg",
+                img_alt: "User Photo: Sarah M."
+            }
+        },
+        {
+            text: "I had a fantastic experience at DentalCare. The staff were friendly and professional, the procedure was quick and painless, and my smile has never looked better. Highly recommend!",
+            author: {
+                name: "John D.",
+                role: "Patient",
+                img_link: "https://randomuser.me/api/portraits/men/78.jpg",
+                img_alt: "User Photo: John D."
+            }
+        },
+        {
+            text: "As a mom, I felt completely at ease bringing my child to DentalCare. The staff were warm and patient, explained every step, and made the visit stress-free. My child left smiling — we couldn't be happier!",
+            author: {
+                name: "Fatima M.",
+                role: "Mother",
+                img_link: "https://randomuser.me/api/portraits/women/68.jpg",
+                img_alt: "User Photo: Fatima M."
+            }
+        },
+        {
+            text: "At 52, I was nervous about visiting the dentist again, but the team treated me like family. They were patient, gentle, and explained everything. I left with a warm smile and a lighter heart — I couldn't be happier!",
+            author: {
+                name: "Alex W.",
+                role: "Patient",
+                img_link: "https://randomuser.me/api/portraits/men/22.jpg",
+                img_alt: "User Photo: Alex W."
+            }
+        }
+    ]
+</script>
 
 <style scoped>
     @import url('../assets/styles/testimonials.css');
